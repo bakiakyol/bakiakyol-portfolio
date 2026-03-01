3b684e31816ae73b3b3201ac2fadd90a7b3828a0
Mustafa Eftekin <meftekin@outlook.com>
2026-03-02 01:32:17 +0300

    chore: standardize quotes in environment and about schema files

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,20 +1,21 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-03-01'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2026-03-01";
 
 export const dataset =
   process.env.NEXT_PUBLIC_SANITY_DATASET ||
   process.env.SANITY_STUDIO_DATASET ||
-  'production'
+  "production";
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || process.env.SANITY_STUDIO_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID (or SANITY_STUDIO_PROJECT_ID)'
-)
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ||
+    process.env.SANITY_STUDIO_PROJECT_ID,
+  "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID (or SANITY_STUDIO_PROJECT_ID)",
+);
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
-    throw new Error(errorMessage)
+    throw new Error(errorMessage);
   }
 
-  return v
+  return v;
 }
